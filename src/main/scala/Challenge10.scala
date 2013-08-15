a46f2ce6691587f3c05bfbf2f4cdcb4cb6900f31
Devon Stewart <blast@hardchee.se>
2013-08-15 20:00:35 +0900

    Challenge13: Run-length encoding of a list (direct solution).
    Whoops, didn't read the directions earlier... didn't use Challenge09.pack in Challenge10, so I rewrote that.

--- src/main/scala/Challenge10.scala
+++ src/main/scala/Challenge10.scala
@@ -2,10 +2,9 @@ package se.hardchee.S99
 
 object Challenge10 extends Challenge {
   def encode[A](iter: Iterable[A]): Iterable[Tuple2[Int, A]] = {
-    iter.foldLeft[List[Tuple2[Int, A]]](List())({
-      case (sofar, next) if(!sofar.isEmpty && sofar.last._2 == next) => sofar.init :+ (sofar.last._1 + 1, next)
-      case (sofar, next) => sofar :+ (1, next)
-    })
+    Challenge09.pack(iter) map {
+      case xs: Iterable[_] => (xs.toList.length, xs.head: A)
+    }
   }
 
   def go = {
